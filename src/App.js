534f85827c6b9aeb9044e8a4b55960946912d17f
ozngrsc <ozangursucu@hotmail.com>
2022-06-09 12:19:16 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Youtube-Clone 🎞🎵📺</h1>
+    </div>
+  );
+}
+
+export default App;
