6223f1f0e9c949613d816f142149dd42f7ea7e2e
ozngrsc <ozangursucu@hotmail.com>
2022-06-13 14:50:27 +0300

    sidebarRow

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,10 +1,28 @@
 import React from "react";
 import "./Sidebar.css";
+import SidebarRow from "./SidebarRow";
+import HomeIcon from "@mui/icons-material/Home";
+import WhatshotIcon from "@mui/icons-material/Whatshot";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import LibraryAddIcon from "@mui/icons-material/LibraryAdd";
+import HistoryIcon from "@mui/icons-material/History";
+import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
+import WatchLaterIcon from "@mui/icons-material/WatchLater";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h2>Sidebar</h2>
+      <SidebarRow Icon={HomeIcon} title="Home" />
+      <SidebarRow Icon={WhatshotIcon} title="Trending" />
+      <SidebarRow Icon={SubscriptionsIcon} title="Subscription" />
+      <SidebarRow Icon={LibraryAddIcon} title="Library" />
+      <SidebarRow Icon={HistoryIcon} title="History" />
+      <SidebarRow Icon={OndemandVideoIcon} title="Your Videos" />
+      <SidebarRow Icon={WatchLaterIcon} title="Watch Later" />
+      <SidebarRow Icon={ThumbUpIcon} title="Liked Videos" />
+      <SidebarRow Icon={ExpandMoreIcon} title="Show More" />
     </div>
   );
 }
