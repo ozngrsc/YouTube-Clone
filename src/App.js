95cce1485ad4486c85e00eb8fd36c91e6a77bdf5
ozngrsc <ozangursucu@hotmail.com>
2022-06-13 12:19:43 +0300

    header style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
+import Sidebar from "./Components/Sidebar/Sidebar";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       {/* Header */}
       <Header />
 
-      {/* Sidebar */}
+      <Sidebar />
 
       {/* Recommended Video */}
     </div>
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -20,12 +20,12 @@ function Header() {
       </div>
       <div className="header__input">
         <input placeholder="Search" type="text" />
-        <SearchIcon />
+        <SearchIcon className="header__inputButton" />
       </div>
       <div className="header__icons">
-        <VideoCallIcon />
-        <AppsIcon />
-        <NotificationsIcon />
+        <VideoCallIcon className="header__icon" />
+        <AppsIcon className="header__icon" />
+        <NotificationsIcon className="header__icon" />
         <Avatar
           src="https://media-exp1.licdn.com/dms/image/C4E03AQEZ5TX0YIhjUw/profile-displayphoto-shrink_800_800/0/1517626990623?e=1660176000&v=beta&t=K8bzdUFf4Zwm3Xo-uQvgyM49g7-6ylxjNniwiBTQSTw"
           alt="Ozan Gürsucu"
