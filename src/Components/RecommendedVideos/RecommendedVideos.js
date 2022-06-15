279b60e87142b0e0d8c8fa26551f1641447ba6cf
ozngrsc <ozangursucu@hotmail.com>
2022-06-15 09:50:36 +0300

    sidebar finished

--- src/Components/RecommendedVideos/RecommendedVideos.js
+++ src/Components/RecommendedVideos/RecommendedVideos.js
@@ -4,7 +4,7 @@ import "./RecommendedVideos.css";
 function RecommendedVideos() {
   return (
     <div className="recommendedVideos">
-      <h2>Recommended Videos</h2>
+      <h2>Recommended</h2>
     </div>
   );
 }
--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -4,11 +4,11 @@ import SidebarRow from "./SidebarRow";
 import HomeIcon from "@mui/icons-material/Home";
 import WhatshotIcon from "@mui/icons-material/Whatshot";
 import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
-import LibraryAddIcon from "@mui/icons-material/LibraryAdd";
+import VideoLibraryIcon from "@mui/icons-material/VideoLibrary";
 import HistoryIcon from "@mui/icons-material/History";
 import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
 import WatchLaterIcon from "@mui/icons-material/WatchLater";
-import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 function Sidebar() {
@@ -17,12 +17,14 @@ function Sidebar() {
       <SidebarRow selected Icon={HomeIcon} title="Home" />
       <SidebarRow Icon={WhatshotIcon} title="Trending" />
       <SidebarRow Icon={SubscriptionsIcon} title="Subscription" />
-      <SidebarRow Icon={LibraryAddIcon} title="Library" />
+      <hr />
+      <SidebarRow Icon={VideoLibraryIcon} title="Library" />
       <SidebarRow Icon={HistoryIcon} title="History" />
       <SidebarRow Icon={OndemandVideoIcon} title="Your Videos" />
       <SidebarRow Icon={WatchLaterIcon} title="Watch Later" />
-      <SidebarRow Icon={ThumbUpIcon} title="Liked Videos" />
+      <SidebarRow Icon={ThumbUpOffAltIcon} title="Liked Videos" />
       <SidebarRow Icon={ExpandMoreIcon} title="Show More" />
+      <hr />
     </div>
   );
 }
