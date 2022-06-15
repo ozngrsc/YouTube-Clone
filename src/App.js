5959daa2355c02d9f859645b14aa6994fa808406
ozngrsc <ozangursucu@hotmail.com>
2022-06-15 14:02:05 +0300

    react-router

--- src/App.js
+++ src/App.js
@@ -3,17 +3,29 @@ import "./App.css";
 import Header from "./Components/Header/Header";
 import RecommendedVideos from "./Components/RecommendedVideos/RecommendedVideos";
 import Sidebar from "./Components/Sidebar/Sidebar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      {/* Header */}
-      <Header />
-      <div className="app__page">
-        <Sidebar />
-
-        <RecommendedVideos />
-      </div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route
+            path="/search/:searchTerm"
+            element={[<h1>Search Page</h1>]}
+          ></Route>
+          <Route
+            path="/"
+            element={[
+              <div className="app__page">
+                <Sidebar />,
+                <RecommendedVideos />
+              </div>,
+            ]}
+          ></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
@@ -6,21 +6,33 @@ import VideoCallIcon from "@mui/icons-material/VideoCall";
 import AppsIcon from "@mui/icons-material/Apps";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import { Avatar } from "@mui/material";
+import { Link } from "react-router-dom";
 
 function Header() {
+  const [inputSearch, setInputSearch] = useState("");
+
   return (
     <div className="header">
       <div className="header__left">
         <MenuIcon />
-        <img
-          className="header__logo"
-          src="https://static.vecteezy.com/system/resources/previews/003/206/623/original/youtube-editorial-app-icon-free-vector.jpg"
-          alt=""
-        />
+        <Link to="/">
+          <img
+            className="header__logo"
+            src="https://static.vecteezy.com/system/resources/previews/003/206/623/original/youtube-editorial-app-icon-free-vector.jpg"
+            alt=""
+          />
+        </Link>
       </div>
       <div className="header__input">
-        <input placeholder="Search" type="text" />
-        <SearchIcon className="header__inputButton" />
+        <input
+          onChange={(e) => setInputSearch(e.target.value)}
+          value={inputSearch}
+          placeholder="Search"
+          type="text"
+        />
+        <Link to={`/search/${inputSearch}`}>
+          <SearchIcon className="header__inputButton" />
+        </Link>
       </div>
       <div className="header__icons">
         <VideoCallIcon className="header__icon" />
